95a5c394e8f3a95f6b94b72136f9030873e3e58d
BelisnalvaCosta <bella-costa@hotmail.com.br>
2023-06-08 20:35:56 -0300

    ACommit initial

---
+++ src/main/java/me/dio/salasDeReuniao/SalasDeReuniaoApplication.java
@@ -0,0 +1,13 @@
+package me.dio.salasDeReuniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalasDeReuniaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalasDeReuniaoApplication.class, args);
+	}
+
+}
