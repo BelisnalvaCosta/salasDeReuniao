04133c8e9398947a37faa50db0c2e2fdc8973927
BelisnalvaCosta <bella-costa@hotmail.com.br>
2023-06-14 12:34:55 -0300

    Committing packages, components Angular and bootstrap Jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { RoomListComponent } from './room-list/room-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent,
+    RoomDetailsComponent,
+    RoomListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/room-details/room-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-details',
+  templateUrl: './room-details.component.html',
+  styleUrls: ['./room-details.component.css']
+})
+export class RoomDetailsComponent {
+
+}
